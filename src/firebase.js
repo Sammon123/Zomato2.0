0e6ca389f861e1cdcefc745190f94b8e955fc286
Peter Sammon <petersammon2@gmail.com>
2020-11-21 16:08:05 -0500

    set up firebase project

---
+++ src/firebase.js
@@ -0,0 +1,11 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyC3k8Fm71hprC8zTvCgJpmR3IfYRxhQLow",
+    authDomain: "zomato-a4bd6.firebaseapp.com",
+    databaseURL: "https://zomato-a4bd6.firebaseio.com",
+    projectId: "zomato-a4bd6",
+    storageBucket: "zomato-a4bd6.appspot.com",
+    messagingSenderId: "829549204508",
+    appId: "1:829549204508:web:0dc3dc0afa0b82a0ae7bd9",
+    measurementId: "G-0GP5E3NH6C"
+};
